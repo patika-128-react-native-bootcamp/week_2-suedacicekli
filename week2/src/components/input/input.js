345df1336e44f85f1266ac5f766f25560604dd41
suedacicekli <suedacicekli@gmail.com>
2021-11-26 17:47:49 +0300

    Update | add buton added & state created for input

--- week2/src/components/input/input.js
+++ week2/src/components/input/input.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Button, TextInput, TouchableOpacity} from 'react-native';
 
 import styles from './input.style';
 const inputt = props => {
+  const [dataName, setdataName] = useState();
+  const [dataPrice, setdataPrice] = useState(0);
+
+  const inputArea = () => {
+    let product = dataName;
+    let price = dataPrice;
+    props.getAddData(product, price);
+  };
   return (
     <View>
       <Text style={styles.textInput}>Name</Text>
-      <TextInput style={styles.input} placeholder="enter name" />
+      <TextInput
+        style={styles.input}
+        placeholder="enter name"
+        onChangeText={setdataName}
+      />
 
       <Text style={styles.textInput}>Price</Text>
-      <TextInput style={styles.input} placeholder="enter price" />
+      <TextInput
+        style={styles.input}
+        placeholder="enter price"
+        onChangeText={setdataPrice}
+      />
+      <View style={styles.AddButton}>
+        <Button
+          title="Add"
+          color="white"
+          style={styles.AddTitle}
+          onPress={inputArea}
+        />
+      </View>
     </View>
   );
 };
